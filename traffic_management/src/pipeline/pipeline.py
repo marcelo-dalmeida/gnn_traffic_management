ec78f7607732dee98c194a398caed1ffd1c60b9e
Marcelo d'Almeida <md@id.uff.br>
2024-04-10 22:33:11 -0400

    Add parallel generation of datasets

--- traffic_management/src/pipeline/pipeline.py
+++ traffic_management/src/pipeline/pipeline.py
@@ -1,5 +1,6 @@
 import os
 import time
+from multiprocessing import Process
 
 import config
 from const import CONST
@@ -41,10 +42,24 @@ class Pipeline:
             file.write(f"{end_t - start_t}\n")
         print(f"testing: {end_t - start_t}\n")
 
-    def generate_dataset(self):
+    def generate_dataset(self, multi_process=True):
 
-        for traffic_pattern in config.EXPERIMENT.TRAFFIC_PATTERNS:
-            dataset_generator.generate_dataset(self.env, traffic_pattern)
+        generator_process_list = []
+        if multi_process:
+            for generator_id, traffic_pattern in enumerate(config.EXPERIMENT.TRAFFIC_PATTERNS):
+                p = Process(target=dataset_generator.generate_dataset,
+                            args=(self.env, traffic_pattern, generator_id)
+                            )
+                p.start()
+                generator_process_list.append(p)
+
+            for i in range(len(generator_process_list)):
+                p = generator_process_list[i]
+                p.join()
+        else:
+            for traffic_pattern in config.EXPERIMENT.TRAFFIC_PATTERNS:
+                dataset_generator.generate_dataset(self.env, traffic_pattern)
+
         dataset_generator.combine_datasets()
         dataset_generator.generate_adjacency_graph(self.env)
         dataset_generator.generate_static_embedding()
