b376d9adfa15cbb0902b9cd7af583748595169e9
Marcelo d'Almeida <md@id.uff.br>
2024-04-11 21:27:15 -0400

    Fix data input

--- traffic_management/src/agent/cgan_gman/utils.py
+++ traffic_management/src/agent/cgan_gman/utils.py
@@ -52,15 +52,48 @@ def calculate_detection_metrics(pred, label, detection_times):
     return dr, fpr, f_score, mttd
 
 
-def seq2instance(data, P, Q):
+def count_continuous_freq(arr):
+    count = 0
+    items = []
+    counts = []
+
+    for i in range(0, len(arr)-1):
+        if arr[i] == arr[i+1]:
+            count += 1
+        else:
+            items.append(arr[i])
+            counts.append(count)
+            count = 0
+
+    items.append(arr[len(arr)-1])
+    counts.append(count)
+
+    return np.array(items), np.array(counts)
+
+
+def seq2instance(data, P, Q, trafpatY):
+
+    items, counts = count_continuous_freq(trafpatY)
+
     num_step, dims = data.shape
-    num_sample = num_step - P - Q + 1
-    x = np.zeros(shape=(num_sample, P, dims))
-    y = np.zeros(shape=(num_sample, Q, dims))
-    for i in range(num_sample):
-        x[i] = data[i: i + P]
-        y[i] = data[i + P: i + P + Q]
-    return x, y
+    num_samples = counts - P - Q + 1
+    x = np.zeros(shape=(sum(num_samples), P, dims))
+    y = np.zeros(shape=(sum(num_samples), Q, dims))
+    trafpatY_label = np.zeros(shape=(sum(num_samples), Q))
+
+    total = 0
+    for num_sample in num_samples:
+        for i in range(num_sample):
+            k = total + i
+            x[k] = data[k: k + P]
+            y[k] = data[k + P: k + P + Q]
+            trafpatY_label[k] = trafpatY[k + P: k + P + Q]
+
+        total += num_sample
+
+    trafpatY_label = np.expand_dims(trafpatY_label, -1)
+
+    return x, y, trafpatY_label
 
 
 def loadData(dataset_file, attribute):
@@ -75,29 +108,35 @@ def loadData(dataset_file, attribute):
     df = pd.read_hdf(os.path.join(config.ROOT_DIR, dataset_file), key='data')
     df = df.reset_index(level=0)
 
-    trafpatY = df.loc[:, 'traffic_pattern'].replace(traffic_patterns).values
+    df = df.sort_index()
 
-    df = df.loc[:, attribute]
+    df.loc[:, 'traffic_pattern'] = df.loc[:, 'traffic_pattern'].replace(traffic_patterns)
 
-    Traffic = df.values
     # train/val/test
-    num_step = df.shape[0]
-    train_steps = round(config.AGENT.TRAIN_RATIO * num_step)
-    test_steps = round(config.AGENT.TEST_RATIO * num_step)
-    val_steps = num_step - train_steps - test_steps
-    train = Traffic[: train_steps]
-    val = Traffic[train_steps: train_steps + val_steps]
-    test = Traffic[-test_steps:]
+    num_step = df.shape[0] // len(config.EXPERIMENT.TRAFFIC_PATTERNS)
+    train_steps = round(config.AGENT.TRAIN_RATIO * num_step) * len(config.EXPERIMENT.TRAFFIC_PATTERNS)
+    test_steps = round(config.AGENT.TEST_RATIO * num_step) * len(config.EXPERIMENT.TRAFFIC_PATTERNS)
+    val_steps = num_step * len(config.EXPERIMENT.TRAFFIC_PATTERNS) - train_steps - test_steps
 
-    traintrafpatY, valtrafpatY, testtrafpatY = trafpatY[: train_steps], trafpatY[train_steps: train_steps + val_steps], trafpatY[-test_steps:]
+    train_data = df[: train_steps].sort_values(by=['traffic_pattern', 'time'])
+    val_data = df[train_steps: train_steps + val_steps].sort_values(by=['traffic_pattern', 'time'])
+    test_data = df[-test_steps:].sort_values(by=['traffic_pattern', 'time'])
+
+    train = train_data.loc[:, attribute].values
+    val = val_data.loc[:, attribute].values
+    test = test_data.loc[:, attribute].values
+
+    traintrafpatY_label = train_data.loc[:, 'traffic_pattern'].values
+    valtrafpatY_label = val_data.loc[:, 'traffic_pattern'].values
+    testtrafpatY_label = test_data.loc[:, 'traffic_pattern'].values
 
     # X, Y
-    trainX, trainY = seq2instance(
-        train, config.AGENT.HISTORY_STEPS, config.AGENT.PREDICTION_STEPS)
-    valX, valY = seq2instance(
-        val, config.AGENT.HISTORY_STEPS, config.AGENT.PREDICTION_STEPS)
-    testX, testY = seq2instance(
-        test, config.AGENT.HISTORY_STEPS, config.AGENT.PREDICTION_STEPS)
+    trainX, trainY, traintrafpatY = seq2instance(
+        train, config.AGENT.HISTORY_STEPS, config.AGENT.PREDICTION_STEPS, traintrafpatY_label)
+    valX, valY, valtrafpatY = seq2instance(
+        val, config.AGENT.HISTORY_STEPS, config.AGENT.PREDICTION_STEPS, valtrafpatY_label)
+    testX, testY, testtrafpatY = seq2instance(
+        test, config.AGENT.HISTORY_STEPS, config.AGENT.PREDICTION_STEPS, testtrafpatY_label)
     # normalization
     mean, std = np.mean(trainX), np.std(trainX)
     trainX = (trainX - mean) / std
@@ -118,26 +157,27 @@ def loadData(dataset_file, attribute):
         SE[index] = temp[1:]
 
     # temporal embedding
-    Time = df.index.drop_duplicates()
+    Time = df.index
     dayofweek = np.reshape(Time.weekday, newshape=(-1, 1))
-    Time.freq = pd.infer_freq(Time) if Time.freq is None else Time.freq
-    timeofday = (Time.hour * 3600 + Time.minute * 60 + Time.second) // Time.freq.delta.total_seconds()
+    no_duplicates_Time = Time.drop_duplicates()
+    no_duplicates_Time.freq = pd.infer_freq(no_duplicates_Time)
+    timeofday = (Time.hour * 3600 + Time.minute * 60 + Time.second) // no_duplicates_Time.freq.delta.total_seconds()
     timeofday = np.reshape(timeofday, newshape=(-1, 1))
     Time = np.concatenate((dayofweek, timeofday), axis=-1)
-    # train/val/test
-    train = Time[:train_steps]
-    val = Time[train_steps: train_steps + val_steps]
-    test = Time[-test_steps:]
+
+    Time = list(zip(Time, df['traffic_pattern'].values))
+
+    train = np.array(list(zip(*sorted(Time[: train_steps], key=lambda x: (x[1], 0))))[0])
+    val = np.array(list(zip(*sorted(Time[train_steps: train_steps + val_steps], key=lambda x: (x[1], 0))))[0])
+    test = np.array(list(zip(*sorted(Time[-test_steps:], key=lambda x: (x[1], 0))))[0])
+
     # shape = (num_sample, P + Q, 2)
-    trainTE = seq2instance(train, config.AGENT.HISTORY_STEPS,
-                           config.AGENT.PREDICTION_STEPS)
-    trainTE = np.concatenate(trainTE, axis=1).astype(np.int32)
-    valTE = seq2instance(val, config.AGENT.HISTORY_STEPS,
-                         config.AGENT.PREDICTION_STEPS)
-    valTE = np.concatenate(valTE, axis=1).astype(np.int32)
-    testTE = seq2instance(test, config.AGENT.HISTORY_STEPS,
-                          config.AGENT.PREDICTION_STEPS)
-    testTE = np.concatenate(testTE, axis=1).astype(np.int32)
+    trainTE_1, trainTE_2, _ = seq2instance(train, config.AGENT.HISTORY_STEPS, config.AGENT.PREDICTION_STEPS, traintrafpatY_label)
+    trainTE = np.concatenate((trainTE_1, trainTE_2), axis=1).astype(np.int32)
+    valTE_1, valTE_2, _ = seq2instance(val, config.AGENT.HISTORY_STEPS, config.AGENT.PREDICTION_STEPS, valtrafpatY_label)
+    valTE = np.concatenate((valTE_1, valTE_2), axis=1).astype(np.int32)
+    testTE_1, testTE_2, _ = seq2instance(test, config.AGENT.HISTORY_STEPS, config.AGENT.PREDICTION_STEPS, testtrafpatY_label)
+    testTE = np.concatenate((testTE_1, testTE_2), axis=1).astype(np.int32)
 
     return (trainX, trainTE, trainY, traintrafpatY,
             valX, valTE, valY, valtrafpatY,
